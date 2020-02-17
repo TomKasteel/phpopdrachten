2b93e47394c4f3ff0b922da100ffb434595ff103
Tom van Mourik <tom.vanmourik@edu-kw1c.nl>
2020-02-17 15:09:10 +0100

    dit is een test

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * User: Tom
+ * Date: 05-02-2020
+ * Time: 2:02 PM
+ * File: index.php
+ */
+?>
+<!doctype html>
+<html>
+<head>
+    <title>Index</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+<header>
+    <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+</header>
+<aside>
+    <h2>Menu</h2>
+    <ul>
+        <li>Hoofdstuk 2
+            <ul>
+                <li>
+                    <a href="hoofdstuk2/opdracht2.1.php">Opdracht 2.1</a>
+                </li>
+                <li>
+                    <a href="hoofdstuk2/opdracht2.2.php">Opdracht 2.2</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</aside>
+<main id="wrapper">
+    <h2>Uitwerkingen</h2>
+</main>
+</body>
+
+</html>
